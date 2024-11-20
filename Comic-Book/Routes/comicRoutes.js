ba2b8f351a1940493e2e732d619e626036b1b52b
Bhupendra Singh <bs4004908@gmail.com>
2024-11-20 22:59:55 +0530

    new commit

---
+++ Comic-Book/Routes/comicRoutes.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const comicController = require('../controllers/comiccontrollers');
+const validateComicBook = require('/middleware/validation');
+const router = express.Router();
+
+
+router.post('/', validateComicBook, comicController.createComicBook);
+router.put('/:id', comicController.updateComicBook);
+router.delete('/:id', comicController.deleteComicBook);
+router.get('/', comicController.getAllComicBooks);
+router.get('/:id', comicController.getComicBook);
+
+module.exports = router;
